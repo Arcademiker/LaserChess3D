02666dfa738d39be11d7a8863946abdf8504187a
Thomas <thomas.engels@arcademiker.de>
2018-07-27 01:14:57 +0200

    Remove duplicated directory

---
+++ objloader.cpp
@@ -0,0 +1,4 @@
+//
+// Created by arcademiker on 26.07.18.
+//
+
---
+++ objloader.h
@@ -0,0 +1,8 @@
+//
+// Created by arcademiker on 26.07.18.
+//
+
+#ifndef LASERCHESS3D_OBJLOADER_H
+#define LASERCHESS3D_OBJLOADER_H
+
+#endif //LASERCHESS3D_OBJLOADER_H
