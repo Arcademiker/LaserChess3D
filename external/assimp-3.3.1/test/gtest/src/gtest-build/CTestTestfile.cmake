acd1a8d8979be97b4c6ee47529cbe994f9d0b682
Thomas <thomas.engels@alumni.fh-aachen.de>
2018-07-31 20:12:31 +0200

    delete gtest

--- external/assimp-3.3.1/test/gtest/src/gtest-build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/arcademiker/CLionProjects/LaserChess3D/external/assimp-3.3.1/test/gtest/src/gtest
-# Build directory: /home/arcademiker/CLionProjects/LaserChess3D/external/assimp-3.3.1/test/gtest/src/gtest-build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs(googlemock)
--- external/assimp-3.3.1/test/gtest/src/gtest-build/googlemock/gtest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/arcademiker/CLionProjects/LaserChess3D/external/assimp-3.3.1/test/gtest/src/gtest/googletest
-# Build directory: /home/arcademiker/CLionProjects/LaserChess3D/external/assimp-3.3.1/test/gtest/src/gtest-build/googlemock/gtest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
